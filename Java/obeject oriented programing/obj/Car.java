ee17899b4dc2d03d1313d0fb2c77987b9b88c8ee
yashpatel427 <yp9321039@gmail.com>
2026-03-02 20:19:23 +0530

    incomplete work

---
+++ Java/obeject oriented programing/obj/Car.java
@@ -0,0 +1,7 @@
+public class Car{
+    String maker = "Ford";
+    String model = "Mustang";
+    int year = 2024;
+    double price = 75000.95;
+    boolean isRunning = false;
+}
---
+++ Java/obeject oriented programing/obj/main.java
@@ -0,0 +1,4 @@
+public class main{
+    Car car = new Car();
+
+}
